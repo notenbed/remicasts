9a5645279c1cf0bac4d31c3f2b807cc0062d126a
remi <remi@remitaylor.com>
2011-04-10 13:56:48 -0700

    Added code from TaskMan screencast

---
+++ ClassLibrary1/ClassLibrary1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("MAIN - begin");
+            TaskMan.Task.Run(args);
+            Console.WriteLine("MAIN - end");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassLibrary1/ClassLibrary1/SomeTasks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskMan;
+
+namespace ClassLibrary1 {
+    public class SomeTasks {
+
+        [Task]
+        public static int Bar()
+        {
+            Console.WriteLine("bar");
+        }
+
+        [Task]
+        public static void X()
+        {
+            Console.WriteLine("x");
+        }
+
+        [Task("this is the first task", Before = "bar no:longer:foo", After = "x")]
+        public static void FirstTask()
+        {
+            Console.WriteLine("first task");
+        }
+
+        [Task("no:longer:foo", "this is the FOO task")]
+        public static void Foo(Variables vars)
+        {
+            Console.WriteLine("no longer foo");
+        }
+
+        [Task]
+        public static void CamelCaseIsFun()
+        {
+            Task.Get("first:task").Run();
+            Console.WriteLine("Camel Case is fun!");
+            Task.Get("first:task").Run();
+        }
+    }
+}
