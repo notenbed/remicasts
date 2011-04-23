d190babb39298130fb26a1b325419fdd55c08652
remi <remi@remitaylor.com>
2011-04-22 19:35:52 -0700

    Working on getting first spec running!

---
+++ TestDrivenDogs.Specs/HomePageSpec.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TestDrivenDogs.Specs {
+
+	[TestFixture]
+	public class HomePageSpec : Requestoring.Requestor {
+
+		[Test]
+		public void should_display_application_title() {
+			Get("http://localhost:50486/");
+
+			Console.WriteLine("Status: {0}", LastResponse.Status);
+			Console.WriteLine("Body: {0}", LastResponse.Body);
+			foreach (var header in LastResponse.Headers)
+				Console.WriteLine("{0}: {1}", header.Key, header.Value);
+		}
+	}
+}
---
+++ TestDrivenDogs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestDrivenDogs.MvcApplication" Language="C#" %>
