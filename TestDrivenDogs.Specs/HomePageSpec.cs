5714f5a6dc8e61af0d24c161538171ff33958637
remi <remi@remitaylor.com>
2011-04-22 19:40:24 -0700

    First green spec!  The application title shows up on the home page!

--- TestDrivenDogs.Specs/HomePageSpec.cs
+++ TestDrivenDogs.Specs/HomePageSpec.cs
@@ -7,16 +7,13 @@ using NUnit.Framework;
 namespace TestDrivenDogs.Specs {
 
 	[TestFixture]
-	public class HomePageSpec : Requestoring.Requestor {
+	public class HomePageSpec : Mara.MaraTest {
 
 		[Test]
 		public void should_display_application_title() {
-			Get("http://localhost:50486/");
-
-			Console.WriteLine("Status: {0}", LastResponse.Status);
-			Console.WriteLine("Body: {0}", LastResponse.Body);
-			foreach (var header in LastResponse.Headers)
-				Console.WriteLine("{0}: {1}", header.Key, header.Value);
+			CurrentDriver = new Mara.Drivers.WebClient();
+			Visit("http://localhost:50486/");
+			Page.Body.ShouldContain("Test-Driven Dogs");
 		}
 	}
 }
